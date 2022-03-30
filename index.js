d41f4b017aee1a52761dc121900da4366b2b825e
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-03-30 11:05:38 +0300

    3.3 puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (!(person)) {
+        return res.status(404).end()
+    }
+    res.json(person)
+})
+
 app.get('/info', (req, res) => {
     res.send(
         `<div>
